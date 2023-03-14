ba206f0dd12f955bb3b5121e995d1192c19f443b
olusegun Owolabi <olujawo1996@gmail.com>
2023-03-14 11:56:53 +0100

    working on counter

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
               <Route path="/" element={<EmailList />}/>
             </Routes>
           </div>
+          <SendMail/>
         </div>
       </>
     </BrowserRouter>
