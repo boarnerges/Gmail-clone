96b40f646deae40a2ebc387d7e7eced126f14d89
olusegun Owolabi <olujawo1996@gmail.com>
2023-03-21 01:56:58 +0100

    React hook form

--- src/SendMail.js
+++ src/SendMail.js
@@ -1,31 +1,62 @@
-import { Close } from '@mui/icons-material'
-import { Button } from '@mui/material'
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import './SendMail.css'
-
+import { Close } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./SendMail.css";
 
 function SendMail() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
-    const {register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data) => {};
+
   return (
-    <div className='sendMail'>
-        <div className = 'sendMail__header'>
-            <h3>New Message</h3>
-            <Close className='sendMail__close'/>
+    <div className="sendMail">
+      <div className="sendMail__header">
+        <h3>New Message</h3>
+        <Close className="sendMail__close" />
+      </div>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name="to"
+          placeholder="To"
+          type="text"
+          ref={register({ required: true })}
+        />
+        {errors.to && <p className="sendMail__error">To is required!</p>}
+
+        <input
+          name="subject"
+          placeholder="Subject"
+          type="text"
+          ref={register({ required: true })}
+        />
+        {errors.subject && (
+          <p className="sendMail__error">Subject is required!</p>
+        )}
+
+        <input
+          name="message"
+          placeholder="Message..."
+          type="text"
+          className="sendMail__message"
+          ref={register({ required: true })}
+        />
+        {errors.message && (
+          <p className="sendMail__error">Message is required!</p>
+        )}
+
+        <div className="sendMail__options">
+          <Button className="sendMail__send">Send</Button>
         </div>
-
-        <form>
-            <input placeholder='To' type='text'/>
-            <input placeholder='Subject' type='text'/>
-            <input placeholder='Message...' type='text' className='sendMail__message'/>
-
-            <div className='sendMail__options'>
-                <Button className='sendMail__send'>Send</Button>
-            </div>
-        </form>
+      </form>
     </div>
-  )
+  );
 }
 
-export default SendMail
+export default SendMail;
